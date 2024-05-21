7a596d7620a95dc0fb96eafa6d0e0f66fe6bb613
milensemkov323@gmail.com <milensemkov323@gmail.com>
2024-05-21 11:50:30 +0300

    Done

---
+++ ShopVegieAndFruitMS/Model/Vegan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopVegieAndFruitMS.Model
+{
+    public class Vegan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public decimal Price { get; set; }
+
+        public int VeganTypesId { get; set; }
+        public VeganType VeganTypes { get; set; } 
+    }
+}
